ef6cc3e74cba628e7f20a8e0850ff9ac5d146c99
singhsanjana <sanjana.singh2906@gmail.com>
2021-09-10 00:30:03 +0530

    (1)removed sb computation (2) added rf computation (3) removed matrix and matrix functionalities

--- code/model_checking_output/pre_calculator/hb.py
+++ code/model_checking_output/pre_calculator/hb.py
@@ -7,71 +7,51 @@ from constants import *
 
 class hb:
 
-	def __init__(self, trace, so_edges, hb_graph, mat_size):
-		# print(trace)
-		self.hb_edges = []	
+	def __init__(self, trace, so_edges):
+		self.hb_edges = []
+		self.rf_edges = []
 		self.so_edges = so_edges
 
-		self.mat = hb_graph
-		self.size = mat_size
-
-		self.matched_reads = []													# global variable used in below recursive function to avoid nested lists
-
-		self.sb(trace)
-		self.sw(trace)
+		# self.sb(trace)
+		self.swrf(trace)
 		
-		self.hb_edges = list(set(self.hb_edges))					# remove duplicates
-		# self.complete_matrix()
+		self.hb_edges = list(set(self.hb_edges))
 
 	def get(self):
-		return self.hb_edges, self.so_edges
+		return self.hb_edges, self.rf_edges, self.so_edges
 
-	# adds all transitive relations
-	# def complete_matrix(self):
-
-	# 	temp = Graph(self.size)
-	# 	temp.adjMatrix = self.mat.adjMatrix
-	# 	flag = 0
-
-	# 	# print("t=",temp.adjMatrix)
-
-	# 	while flag!=2:
-	# 		for i in range(self.size):
-	# 			v1 = i
-	# 			for j in range(self.size):
-	# 				if(self.mat.containsEdge(v1,j)):
-	# 					for k in range(self.size):
-	# 						if(self.mat.containsEdge(j,k)):
-	# 							self.mat.addEdge(v1,k)
-
-	# 		if(temp.adjMatrix == self.mat.adjMatrix):
-	# 			flag += 1
-
+	# [snj]: function not in use
 	def sb(self, trace):
 		# getting a list of sb as tuples of two
 		for i in range(len(trace)-1):
 			if trace[i][T_NO] == trace[i+1][T_NO]:
-				# rule sw
 				self.hb_edges.append((trace[i][S_NO],trace[i+1][S_NO]))
-				self.mat.addEdge(trace[i][S_NO],trace[i+1][S_NO])
-				# no need to add so_edges since sb will be recomputed after fence addition anyway
-
-	def sw(self,trace):
+			
+	def swrf(self,trace):
 		for i in range(1, len(trace)): # 1st event is not hb after any event 
+			# read rf (if any)
+			if trace[i][RF] != 'NA': # has rf details
+				v1 = trace[i][RF]
+				v2 = trace[i][S_NO]
+				self.rf_edges.append((v1, v2))
+
+				if self.get_mem_order(trace, v1) == SEQ_CST and trace[i][MO] == SEQ_CST:
+					self.so_edges.append((v1,v2))
+
+			# read sw using CV
 			for j in range(len(trace[i][CV])):
-				if j+1 == trace[i][T_NO]: # sb computed in another function (valid threads start at 1)
+				if j+1 == trace[i][T_NO]: # sb computed after inserting fences (valid threads start at 1)
 					continue
 
 				if trace[i][CV][j] == 0: # lack of information on hb (program threads start from 1 not 0)
 					continue
 
 				if trace[i-1][T_NO] == trace[i][T_NO]: 
-					if len(trace[i-1][CV]) == len(trace[i][CV]) and trace[i-1][CV][j] == trace[i][CV][j]: # a transitive hb relation
-						continue
+					if len(trace[i-1][CV]) > j and trace[i-1][CV][j] == trace[i][CV][j]: # a transitive hb relation
+							continue
 
 				v1 = int(trace[i][CV][j])
 				v2 = int(trace[i][S_NO])
-				# self.mat.addEdge(v1,v2)
 				self.hb_edges.append((v1,v2))
 				
 				if (self.get_mem_order(trace, v1) == SEQ_CST and trace[i][MO] == SEQ_CST):
