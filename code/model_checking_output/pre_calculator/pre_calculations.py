0dfb37330ac1b3fcc627122c81d02ec0633f70ff
singhsanjana <sanjana.singh2906@gmail.com>
2021-09-10 00:31:01 +0530

    (1) removed unnecessary features, (2) trimmed to and from information from hb and mo, (3) collecting rf from hb

--- code/model_checking_output/pre_calculator/pre_calculations.py
+++ code/model_checking_output/pre_calculator/pre_calculations.py
@@ -5,22 +5,14 @@ from constants import *
 
 def pre_calculations(trace, buggy_trace_no):
 	so_edges = []
-	mat_size = max(map(lambda x: x[0], trace))			# number of instructions in the execution trace
-	mat_size += 1
-	initial_mat = Graph(mat_size)
 
-	for t in trace:
-		print(t)
-
-	hb_calc = hb(trace, so_edges, initial_mat, mat_size)
-	hb_edges, so_edges = hb_calc.get()
-	# print("hb_edges==",hb_edges)
-
+	hb_calc = hb(trace, so_edges)
+	hb_edges, rf_edges, so_edges = hb_calc.get()
+	
 	mo_calc = mo(trace, so_edges, buggy_trace_no)
 	mo_edges, so_edges = mo_calc.get()
-	# print("mo_edges==",mo_edges)
-
-	return hb_edges, mo_edges, so_edges
+	
+	return hb_edges, mo_edges, rf_edges, so_edges
 
 def preprocessing(trace):
 	writes = []
