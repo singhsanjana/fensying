fcb257f770451cd7ddb6b1085ffda7108e534af9
singhsanjana <sanjana.singh2906@gmail.com>
2021-10-28 00:00:35 +0530

    added litmus tests for weak fensying

--- code/litmus/weak_fensying/mo_rf_hb_rfinv.cc
+++ code/litmus/weak_fensying/mo_rf_hb_rfinv.cc
@@ -1,50 +1,44 @@
-#include <stdio.h>
+#include <iostream>
 #include <threads.h>
-#include <stdatomic.h>
+#include <atomic>
+#include <model-assert.h>
 
-#include "librace.h"
-#include "model-assert.h"
+using namespace std;
 
-atomic_int x;
-atomic_int y;
-int a, b, c;
+atomic<int> x ;
+atomic<int> y ;
+int a,b,c;
 
-static void fn1(void *obj)
-{
-	atomic_store_explicit(__LINE__, &x, 1, memory_order_relaxed);
-    atomic_store_explicit(__LINE__, &y, 1, memory_order_relaxed);
+static void t1(void *arg) {
+	x.store(__LINE__, 1, memory_order_relaxed);
 }
 
-static void fn2(void *obj)
-{
-    a=atomic_load_explicit(__LINE__, &y, memory_order_relaxed);
-	atomic_store_explicit(__LINE__, &x, 2, memory_order_relaxed);
+static void t2(void *arg) {
+  a = x.load(__LINE__, memory_order_relaxed);
+  y.store(__LINE__, 1, memory_order_relaxed);
 }
 
-static void fn3(void *obj)
-{
-	
-	b=atomic_load_explicit(__LINE__, &x, memory_order_relaxed);
-	c=atomic_load_explicit(__LINE__, &x, memory_order_relaxed);
+static void t3(void *arg) {
+  b = y.load(__LINE__, memory_order_relaxed);
+  c = x.load(__LINE__, memory_order_relaxed);
 }
 
-int user_main(int argc, char **argv)
-{
-	thrd_t t1, t2, t3;
+int user_main(int argc, char **argv) {
+    thrd_t th1, th2, th3;
 
-	atomic_init(&x, 0);
-	atomic_init(&y, 0);
-	a = b = c = 0;
+    atomic_init(&x, 0);
+    atomic_init(&y, 0);
+    a = b = c = 0;
 
-	thrd_create(&t1, (thrd_start_t)&fn1, NULL);
-	thrd_create(&t2, (thrd_start_t)&fn2, NULL);
-	thrd_create(&t3, (thrd_start_t)&fn3, NULL);
+    thrd_create(&th1, (thrd_start_t)&t1, NULL);
+    thrd_create(&th2, (thrd_start_t)&t2, NULL);
+    thrd_create(&th3, (thrd_start_t)&t3, NULL);
 
-	thrd_join(t1);
-	thrd_join(t2);
-	thrd_join(t3);
+    thrd_join(th1);
+    thrd_join(th2);
+    thrd_join(th3);
 
-	MODEL_ASSERT(!(a==1 && b==2 && c==1));
+    MODEL_ASSERT(!(a == 1 && b == 1 && c == 0));
 
-	return 0;
+    return 0;
 }
