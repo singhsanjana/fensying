0d500eb6c06253fefc1ad60092fe81ce4e628158
singhsanjana <sanjana.singh2906@gmail.com>
2022-01-11 00:42:33 +0530

    1. lots of pretty printing 2. flag to print synthesis summary

--- code/main.py
+++ code/main.py
@@ -14,6 +14,7 @@ import shlex
 import time
 import sys
 
+import publish_result as res
 from constants import output_colours as oc
 from constants import file_info as fi
 from processing import Processing
@@ -30,14 +31,14 @@ parser.add_argument("--traces", "-t", type=int, required=False, dest="no_traces"
 					help="Batch buggy traces (non-optimal).")
 parser.add_argument("--max-iter", "-m", type=int, required=False, dest="max_iter",
 					help="Max number of batches. Used with -t flag.")
-parser.add_argument("--no-outfile", "-n", required=False, action='store_true', dest="no_outfile",
-					help="If used, fences location are printed and transformed file is not generated.")
+parser.add_argument("--synthesis-summary", "-s", required=False, action='store_true', dest="print_synthesis_summary",
+					help="Print the file wise summary of fences synthesized and strengthened.")
 
 args = parser.parse_args()
 filename = args.file											# gets the input file name
 no_traces = args.no_traces										# gets the input number of traces to be checked
 max_iter = args.max_iter										# gets the input maximum number of iterations
-no_outfile = args.no_outfile                                    # if set then output file not generated
+print_synthesis_summary = args.print_synthesis_summary          # print summarya of synthesis if set
 
 
 if not os.path.exists(filename):
@@ -54,9 +55,10 @@ mc_total = 0
 z3_total = 0
 fences_added = 0
 fences_modified = 0
-fence_tags_final = []
+fence_tags_final = {}
 total_iter = 0
 error_string = ""
+synthesis_summary = ""
 
 def fn_main(filename):
 	global mc_total
@@ -102,16 +104,14 @@ def fn_main(filename):
 
 			fences_added += len(req_fences)
 			fences_modified += count_modified_fences
-			fence_tags_final += list(fence_tags.values())
+			if print_synthesis_summary:
+				fence_tags_final.update(fence_tags)
 
 	mc_total += mc_time
 	z3_total += z3_time
 	if no_traces:
-		print("Time- CEG:\t\t",round(mc_time+pre_calc_total, 2)) # CDS time + relations cpmputation time
 		if no_buggy_execs and not error_string:
-			print("Time- Z3:\t\t",round(z3_time, 2))
-			print("Fences synthesized: \t",len(req_fences))
-			print("Fences strengthened:\t",count_modified_fences)
+			res.iteration_result_summary((mc_time+pre_calc_total), z3_time, len(req_fences), count_modified_fences)
 
 	if no_traces and no_buggy_execs and not error_string:
 		fn_main(new_filename)
@@ -126,21 +126,4 @@ except RuntimeError:
 	print(oc.BOLD + oc.FAIL + "\nTool time exceeded 15 minutes.\n" + oc.ENDC)
 	sys.exit(0)
 
-print(oc.OKBLUE + oc.BOLD + "\n\n================= RESULT SUMMARY =================" + oc.ENDC)
-if not error_string:
-	# TODO: how to compute order of added fences if some existing fence is modified
-	print(oc.OKGREEN, oc.BOLD, "Total fences synthesized:  \t", fences_added, oc.ENDC)
-	print(oc.OKGREEN, oc.BOLD, "Total fences strengthened: \t", fences_modified, oc.ENDC)
-
-print("Time- CEG:\t\t",round(mc_total+pre_calc_total, 2)) # CDS time + relations cpmputation time
-if z3_total > 0:
-	print("Time- Z3:\t\t",round(z3_total, 2))
-print("\nTime- Total:\t\t",round(end-start, 2))
-print("Time- Fensying:\t\t",round(end-start-mc_total-pre_calc_total-z3_total, 2))
-if no_traces:
-	print("\nTotal iterations:\t",total_iter)
-	print("Time- avg per iter:\t",round((end-start)/total_iter, 2))
-
-if z3_total > 0:
-	fenced_filename = filename[:-3] + fi.OUTPUT_FILE_APPEND_STRING
-	print(oc.OKBLUE, oc.BOLD, "\n\nFixed program at:", fenced_filename, "\n", oc.ENDC)
+res.final_result_summary((end-start), (mc_total+pre_calc_total), z3_total, fences_added, fences_modified, no_traces, total_iter, filename, print_synthesis_summary, fence_tags_final)
