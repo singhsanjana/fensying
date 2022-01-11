bec089c2427b1ab77525f192ad3f8d7f5b9f8cf1
singhsanjana <sanjana.singh2906@gmail.com>
2022-01-11 22:00:04 +0530

    change in dir structure

--- test/.wrir.o.d
+++
@@ -1,6 +0,0 @@
-wrir.o: wrir.cc ../model-checker/cds-checker/include/threads.h \
- ../model-checker/cds-checker/include/atomic \
- ../model-checker/cds-checker/include/impatomic.h \
- ../model-checker/cds-checker/include/memoryorder.h \
- ../model-checker/cds-checker/include/cmodelint.h \
- ../model-checker/cds-checker/include/model-assert.h
--- test/.wrir_fixed.o.d
+++
@@ -1,7 +0,0 @@
-wrir_fixed.o: wrir_fixed.cc \
- ../model-checker/cds-checker/include/threads.h \
- ../model-checker/cds-checker/include/atomic \
- ../model-checker/cds-checker/include/impatomic.h \
- ../model-checker/cds-checker/include/memoryorder.h \
- ../model-checker/cds-checker/include/cmodelint.h \
- ../model-checker/cds-checker/include/model-assert.h
