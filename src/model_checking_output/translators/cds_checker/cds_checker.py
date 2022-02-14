13e759c4677d9dccdc60b7480913550f1241206d
dvynjli <dvynjli@gmail.com>
2022-02-14 15:17:23 +0530

    termination on fensying TO

--- src/model_checking_output/translators/cds_checker/cds_checker.py
+++ src/model_checking_output/translators/cds_checker/cds_checker.py
@@ -73,6 +73,7 @@ class translate_cds:
 			print(e)
 		else:
 			cds_end = time.time()
+			signal.signal(signal.SIGALRM, time_handler)
 			signal.alarm(900)								# set timer for 15 minutes for the rest of the tool
 			p = p.decode('utf-8', errors='ignore')			# convert to string
 			self.cds_time = cds_end - cds_start
