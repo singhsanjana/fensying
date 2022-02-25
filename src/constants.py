302848796eba1f4da8fd6aac66eed0992b1b77ce
singhsanjana <sanjana.singh2906@gmail.com>
2022-02-26 03:06:44 +0530

    added std:: to fences to be inserted for files that don't have using namespace std

--- src/constants.py
+++ src/constants.py
@@ -45,10 +45,10 @@ class file_info:
 	CDS_MADE_FILE_EXTENSIONS = [".o", ".o.d"]
 	OUTPUT_FILE_APPEND_STRING = "_fixed"
 	FENCE_INSTRUCTIONS = {
-		f_tags.sc	:	"atomic_thread_fence(__FILE__, __LINE__, memory_order_seq_cst);\n",
-		f_tags.a	:	"atomic_thread_fence(__FILE__, __LINE__, memory_order_acquire);\n",
-		f_tags.r	:	"atomic_thread_fence(__FILE__, __LINE__, memory_order_release);\n",
-		f_tags.ar	:	"atomic_thread_fence(__FILE__, __LINE__, memory_order_acq_rel);\n"
+		f_tags.sc	:	"atomic_thread_fence(__FILE__, __LINE__, std::memory_order_seq_cst);\n",
+		f_tags.a	:	"atomic_thread_fence(__FILE__, __LINE__, std::memory_order_acquire);\n",
+		f_tags.r	:	"atomic_thread_fence(__FILE__, __LINE__, std::memory_order_release);\n",
+		f_tags.ar	:	"atomic_thread_fence(__FILE__, __LINE__, std::memory_order_acq_rel);\n"
 	}
 	CDS_FOLDER_PATH = "../model-checker/cds-checker"
 	TEST_FOLDER_PATH = "test"
