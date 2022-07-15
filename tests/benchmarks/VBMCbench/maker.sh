f5c2daedaa89cce745eb151d44038e4caf854e3e
dvynjli <dvynjli@gmail.com>
2022-07-15 14:45:49 +0530

    deleted unused benchmarks

--- tests/benchmarks/VBMCbench/maker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ -MMD -MF ./.dekker_simple.o.d -o dekker_simple.o dekker_simple.cc -Wall -g -O3 -I../../../model-checker/cds-checker -I../../../model-checker/cds-checker/include -fpermissive -L../../../model-checker/cds-checker -lmodel
-g++ -MMD -MF ./.peterson.o.d -o peterson.o peterson.cc -Wall -g -O3 -I../../../model-checker/cds-checker -I../../../model-checker/cds-checker/include -fpermissive -L../../../model-checker/cds-checker -lmodel
